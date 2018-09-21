1e4abe1ec564179abf16994772e650163fb35b4d
zsamotw <donkemot@interia.pl>
2018-09-21 10:37:50 +0200

    Improve def loop_for_new_boards in Board.

--- board.py
+++ board.py
@@ -10,3 +10,16 @@ class Board:
         return all([(not other.is_attack(figure.x, figure.y)
                      and not figure.is_attack(other.x, other.y))
                     for other in self.figures])
+
+    def loop_for_new_boards(self, figure_class):
+        boards = []
+        for x in range(1, self.size + 1):
+            for y in range(1, self.size + 1):
+                figure = figure_class(x, y)
+                print(self.is_safe_for(figure))
+                if self.is_safe_for(figure):
+                    board = Board(self.size)
+                    figures = self.figures[:].append(figure)
+                    board.figures = figures
+                    boards.append(board)
+        return boards
--- tests/test_board.py
+++ tests/test_board.py
@@ -40,3 +40,23 @@ class TestBoard:
         board.figures = [Knight(1, 1), Bishop(1, 5), King(5, 5)]
         rook = Rook(5, 4)
         assert board.is_safe_for(rook) is False
+
+    def test_loop_for_new_boards_over_empty_board(self):
+        board = Board(2)
+        rook = Rook
+        boards = board.loop_for_new_boards(rook)
+        assert len(boards) is 4
+
+    def test_loop_for_new_boards_over_board_with_Knight(self):
+        board = Board(3)
+        board.figures = [Knight(1, 1)]
+        rook = Rook
+        boards = board.loop_for_new_boards(rook)
+        assert len(boards) is 2
+
+    def test_loop_for_new_boards_over_board_with_two_rooks(self):
+        board = Board(5)
+        board.figures = [Rook(1, 1), Rook(5, 5)]
+        rook = Rook
+        boards = board.loop_for_new_boards(rook)
+        assert len(boards) is 9
